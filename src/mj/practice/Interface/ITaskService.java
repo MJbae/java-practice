7d20e70c1bed4785d64bc33c55921ebbfa9940b0
MJbae <gentlemmj@gmail.com>
2022-04-26 10:11:49 +0900

    feat: define ITaskService

---
+++ src/mj/practice/Interface/ITaskService.java
@@ -0,0 +1,13 @@
+package mj.practice.Interface;
+
+import mj.practice.TaskDto;
+
+import java.util.List;
+
+public interface ITaskService {
+    List<TaskDto> showAll();
+    List<TaskDto> showByCategory(TaskDto task);
+    TaskDto create(TaskDto task);
+    TaskDto update(TaskDto task);
+    TaskDto delete(TaskDto task);
+}
