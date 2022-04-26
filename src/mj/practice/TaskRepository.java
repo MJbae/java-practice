bfa4dcbc0be26ab24d3be4ebb8ea9eb953f303df
MJbae <bmj5208@gmail.com>
2022-04-26 22:05:35 +0900

    feat: implement TaskRepository class only with findAllByCompletedFalse

---
+++ src/mj/practice/TaskRepository.java
@@ -0,0 +1,33 @@
+package mj.practice;
+
+import mj.practice.Interface.ITaskRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TaskRepository implements ITaskRepository {
+    Map<Integer, Category> categories = new HashMap<>();
+    Map<Integer, Task> tasks = new HashMap<>();
+
+    TaskRepository(){
+        Category personalCategory = new Category(0, "Personal");
+        Category businessCategory = new Category(1, "Business");
+        categories.put(personalCategory.getId(), personalCategory);
+        categories.put(businessCategory.getId(), businessCategory);
+
+        Task helloTask = new Task(999, "Hello");
+        Task worldTask = new Task(998, "World");
+        Task byeTask = new Task(997, "Bye");
+        tasks.put(personalCategory.getId(), helloTask);
+        tasks.put(personalCategory.getId(), worldTask);
+        tasks.put(businessCategory.getId(), byeTask);
+    }
+
+    @Override
+    public List<Task> findAllByCompletedFalse() {
+        return tasks.values().stream()
+                .filter(task -> !task.isCompleted())
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+}
