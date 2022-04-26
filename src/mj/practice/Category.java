b13d5a1cd9667f61bcb2a52ea1450c31722ce9b8
MJbae <bmj5208@gmail.com>
2022-04-26 22:04:37 +0900

    feat: implement Category class

--- src/mj/practice/Category.java
+++ src/mj/practice/Category.java
@@ -3,4 +3,31 @@ package mj.practice;
 import mj.practice.Interface.ICategory;
 
 public class Category implements ICategory {
+    private Integer id;
+    private String title;
+
+    public Category(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
 }
