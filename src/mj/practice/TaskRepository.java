7679c63327d69b5983899cda45c6f7794ebb19c3
MJbae <gentlemmj@gmail.com>
2022-04-28 09:09:39 +0900

    refactor: extract initializing logic in constructor

--- src/mj/practice/TaskRepository.java
+++ src/mj/practice/TaskRepository.java
@@ -8,20 +8,7 @@ public class TaskRepository implements ITaskRepository {
     Map<Category, List<Task>> categories = new HashMap<>();
 
     TaskRepository(){
-        Category personalCategory = new Category(0, "Personal");
-        Category businessCategory = new Category(1, "Business");
-        categories.put(personalCategory, new LinkedList<>());
-        categories.put(businessCategory, new LinkedList<>());
-
-        Task helloTask = new Task(999, "Hello");
-        helloTask.complete();
-        Task worldTask = new Task(998, "World");
-        Task byeTask = new Task(997, "Bye");
-        List<Task> personalCategoryTasks = categories.get(personalCategory);
-        List<Task> businessCategoryTasks = categories.get(businessCategory);
-        personalCategoryTasks.add(helloTask);
-        personalCategoryTasks.add(worldTask);
-        businessCategoryTasks.add(byeTask);
+        initialize_categories();
     }
 
     @Override
@@ -66,5 +53,22 @@ public class TaskRepository implements ITaskRepository {
     @Override
     public Task delete(Integer categoryId, Task task) {
         return null;
+    }
+
+    private void initialize_categories() {
+        Category personalCategory = new Category(0, "Personal");
+        Category businessCategory = new Category(1, "Business");
+        categories.put(personalCategory, new LinkedList<>());
+        categories.put(businessCategory, new LinkedList<>());
+
+        Task helloTask = new Task(999, "Hello");
+        helloTask.complete();
+        Task worldTask = new Task(998, "World");
+        Task byeTask = new Task(997, "Bye");
+        List<Task> personalCategoryTasks = categories.get(personalCategory);
+        List<Task> businessCategoryTasks = categories.get(businessCategory);
+        personalCategoryTasks.add(helloTask);
+        personalCategoryTasks.add(worldTask);
+        businessCategoryTasks.add(byeTask);
     }
 }
